45ebcd10e04a496c44f04ad5006e16369db3a76d
Cherrett <cherrettdaniel@gmail.com>
2020-11-03 16:11:56 +0100

    + Updated docs

--- CPP/docs/conf.py
+++ CPP/docs/conf.py
@@ -57,9 +57,12 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'classic'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Example configuration for intersphinx: refer to the Python standard library.
+intersphinx_mapping = {'https://docs.python.org/': None}
--- CPP/docs/index.rst
+++ CPP/docs/index.rst
@@ -1,15 +1,9 @@
-.. DFA Toolkit documentation master file, created by
-   sphinx-quickstart on Mon Nov  2 08:59:04 2020.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
-Welcome to DFA Toolkit's documentation!
+DFA Toolkit Documentation
 =======================================
 
 .. toctree::
    :maxdepth: 2
-   :caption: Contents:
-   
+
    DFA_Toolkit
 
 
